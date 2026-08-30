675b9dba2e6af1cbd001ed2bc568bf226563a0c0
XuanLiangG <gohxl-pm23@student.tarc.edu.my>
2026-08-30 16:57:45 +0800

    resolve some issue

---
+++ lib/features/bike/bike_report.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BikeReportPage extends StatelessWidget {
+  const BikeReportPage({
+    super.key,
+    required this.bikeId,
+  });
+
+  final String bikeId;
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
