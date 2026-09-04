14616b5dac208d60d6a844ee1b0726efcc6c309e
XuanLiangG <gohxl-pm23@student.tarc.edu.my>
2026-09-04 12:25:57 +0800

    Bike performance metric

---
+++ lib/features/bike/models/bike_performance.dart
@@ -0,0 +1,9 @@
+class BikePerformance {
+  final int rentalCount;
+  final double totalDistanceKm;
+
+  const BikePerformance({
+    required this.rentalCount,
+    required this.totalDistanceKm,
+  });
+}
